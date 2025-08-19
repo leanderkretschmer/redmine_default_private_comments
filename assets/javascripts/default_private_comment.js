eabb5d731ef424df9bd9da4a9301d4995c4c2215
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2025-08-19 14:10:38 +0200

    Update default_private_comment.js

--- assets/javascripts/default_private_comment.js
+++ assets/javascripts/default_private_comment.js
@@ -1,1 +1,7 @@
-t
+// default_private_comment.js
+document.addEventListener("DOMContentLoaded", function () {
+  var checkbox = document.getElementById("issue_private_notes");
+  if (checkbox && !checkbox.checked) {
+    checkbox.checked = true;
+  }
+});
