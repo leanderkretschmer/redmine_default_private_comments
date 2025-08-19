3db8cf160e96c3b8d516ea1af02015d4c11d313b
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2025-08-19 14:10:55 +0200

    Update labelled_form_builder_patch.rb

--- lib/redmine_default_private_comments/labelled_form_builder_patch.rb
+++ lib/redmine_default_private_comments/labelled_form_builder_patch.rb
@@ -1,1 +1,15 @@
-
+# lib/redmine_default_private_comments/labelled_form_builder_patch.rb
+module RedmineDefaultPrivateComments
+  module LabelledFormBuilderPatch
+    def check_box(field, options = {}, checked_value = "1", unchecked_value = "0")
+      if @object_name.to_s == "issue" && field.to_s == "private_notes"
+        options = options.symbolize_keys
+        issue = @template.instance_variable_get(:@issue) rescue nil
+        if issue && issue.respond_to?(:private_notes_or_default)
+          options[:checked] = issue.private_notes_or_default
+        end
+      end
+      super(field, options.except(:label), checked_value, unchecked_value)
+    end
+  end
+end
