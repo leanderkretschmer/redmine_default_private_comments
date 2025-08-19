67dc4cd19032eba2278224bcafcda36d4b0ce772
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2025-08-19 14:11:28 +0200

    Update journal_patch.rb

--- lib/redmine_default_private_comments/journal_patch.rb
+++ lib/redmine_default_private_comments/journal_patch.rb
@@ -1,1 +1,8 @@
-
+# lib/redmine_default_private_comments/journal_patch.rb
+module RedmineDefaultPrivateComments
+  module JournalPatch
+    def private_notes_or_default
+      @current_journal&.private_notes || User.current.allowed_to?(:set_notes_private, project)
+    end
+  end
+end
