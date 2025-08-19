15c0ea0a1522af95e8801b9206a94bee130aedd0
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2025-08-19 14:11:46 +0200

    Update patches.rb

--- lib/redmine_default_private_comments/patches.rb
+++ lib/redmine_default_private_comments/patches.rb
@@ -1,1 +1,14 @@
+# lib/redmine_default_private_comments/patches.rb
+require_relative 'issue_patch'
+require_relative 'journal_patch'
+require_relative 'labelled_form_builder_patch'
 
+module RedmineDefaultPrivateComments
+  module Patches
+    def self.apply
+      Issue.prepend IssuePatch if defined?(Issue)
+      Journal.prepend JournalPatch if defined?(Journal)
+      Redmine::Views::LabelledFormBuilder.prepend LabelledFormBuilderPatch
+    end
+  end
+end
